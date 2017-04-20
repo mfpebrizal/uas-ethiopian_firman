886db59596c276deee760fe01bdc438c78cb5198
mfpebrizal <ftm.pebrizal@gmail.com>
2017-04-20 13:20:06 +0700

    user routes

---
+++ server/routes/users.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router()
+var user = require('../controllers/user')
+
+router.post('/', user.create)
+router.get('/', user.findAll)
+router.get('/:id', user.findById)
+router.put('/:id', user.update)
+router.delete('/:id', user.delete)
+
+module.exports = router
